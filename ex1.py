b84a45a6aca58ffe14d2f9d288bbe62538d6cf69
Hansoluo <shy757@126.com>
2017-07-29 21:50:22 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+print "Hello world!" # 你好世界
+print "Hello Again"  # 你好再次
+print "I like typing this."  # 我喜欢敲代码
+print "This is fun."  # 编程太好玩了 
+print 'Yay! Printing.'  # 这是一个单引号引起来的字符串。 
+                        # 哟，打印。
+print "I'd much rather you 'not'."  # 我希望你不要这样
+                                    # 单引号引起来的字符串里有双引号
+print 'I "said" do not touch this.' # 我说了不准碰
+                                    # 双引号引起来的字符串里有单引号
+
+print "Print one more 'line'."  # 附加练习里的多打印一行
