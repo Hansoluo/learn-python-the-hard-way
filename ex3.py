41882f81c02d9b25b8859933bbb6b5777e0fc017
Hansoluo <shy757@126.com>
2017-07-29 23:24:10 +0800

    ex3
    ex3

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6  # 先除法再加法
+print "Roosters", 100 - 25 * 3 % 4  # 先乘法再取余再减法
+
+print "I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 除法和取余先算，再按顺序加减
+
+print "Is it true that 3 + 2 < 5 - 7?"  # 先运算完再进行比较
+
+print 3 + 2 < 5 - 7  # 布尔运算返回True 或者 False
+
+print "What is 3 + 2?", 3 + 2  # 可以直接打印数值运算，用逗号分隔
+print "What is 5 - 7?", 5 - 7 
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2  # 可以直接打印逻辑运算，用逗号分隔
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+
