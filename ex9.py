f2228bbf7e54a6e33f127a455a70e8c8d1b2a5c3
Hansoluo <shy757@126.com>
2017-07-30 09:48:23 +0800

    ex9

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 没有附加练习
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun"  # 转义序列之换行符\n
+
+print days
+print months
+
+print """
+There's something going on here.
+hahahahaha.
+"""
