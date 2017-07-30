ca29e3895fdd208347f53ae7dd8c8112b5678dc3
Hansoluo <shy757@126.com>
2017-07-30 09:25:46 +0800

    ex7

---
+++ ex7.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# 这是最无聊的习题了，直接跳过
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+
+print "." * 10  # 字符串可以直接运算
+
+w = 'This is...'  # 这一段很无聊
+e = 'bored.'
+
+print w + e
+
+
+
+
+
+
+
+
+
